65ff7cea2cc078ca93b3ddfc45eea20cb7629526
Matt Stine <matt.stine@gmail.com>
2022-09-09 07:22:43 -0500

    Finish specing out Project UI

--- src/main/kotlin/com/mattstine/para/cmd/Project.kt
+++ src/main/kotlin/com/mattstine/para/cmd/Project.kt
@@ -1,39 +1,136 @@
 package com.mattstine.para.cmd
 
 import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import java.time.LocalDate
+import java.util.*
 
 @Command(
     name = "project",
     aliases = ["p"],
-    description = ["manages projects"]
+    description = ["Subcommands for managing Projects"],
+    mixinStandardHelpOptions = true
 )
 class Project {
 
     @Command(
+        name = "list",
+        aliases = ["l"],
+        description = ["List Projects"],
+        mixinStandardHelpOptions = true
+    )
+    fun list() {}
+
+    @Command(
         name = "create",
         aliases = ["c"],
-        description = ["creates a new project"],
+        description = ["Creates new Project"],
+        mixinStandardHelpOptions = true
     )
-    fun create() {}
+    fun create(
+        @Option(
+            names = ["-n", "--name"],
+            description = ["Project name"],
+            required = true,
+            paramLabel = "<NAME>"
+        )
+        name: String,
+        @Option(
+            names = ["-s", "--slug"],
+            description = [
+                "Project slug",
+                "Must be unique within PARA Root",
+                "Default will be <NAME> in lower-snake-case"
+                          ],
+            paramLabel = "<SLUG>"
+        )
+        slug: Optional<String>,
+        @Option(
+            names = ["-d", "--review-date"],
+            description = ["Project review date"],
+            required = true,
+            paramLabel = "<yyyy-MM-dd>"
+        )
+        reviewDate: LocalDate,
+        @Option(
+            names = ["-g", "--goals"],
+            description = [
+                "Project goal(s)",
+                "(Optional)"
+                          ],
+            paramLabel = "<GOAL>",
+            split = ","
+        )
+        goal: List<String>?
+    ) {}
+
+    @Command(
+        name = "remove",
+        aliases = ["r"],
+        description = ["Remove Project (no files deleted)"],
+        mixinStandardHelpOptions = true
+    )
+    fun remove(
+        @Option(
+            names = ["-s", "--slug"],
+            description = ["Project slug"],
+            required = true,
+            paramLabel = "<SLUG>"
+        )
+        slug: String
+    ) {}
 
     @Command(
         name = "open",
         aliases = ["o"],
-        description = ["opens project in configured note-taking tool"],
+        description = ["Opens Project in configured PKM Tool"],
+        mixinStandardHelpOptions = true
     )
-    fun open() {}
+    fun open(
+        @Option(
+            names = ["-s", "--slug"],
+            description = [
+                "Project slug",
+            ],
+            paramLabel = "<SLUG>",
+            required = true
+        )
+        slug: String
+    ) {}
 
     @Command(
         name = "cd",
-        description = ["navigates to the project directory"],
+        description = ["Navigates to Project directory"],
+        mixinStandardHelpOptions = true
     )
-    fun cd() {}
+    fun cd(
+        @Option(
+            names = ["-s", "--slug"],
+            description = [
+                "Project slug",
+            ],
+            paramLabel = "<SLUG>",
+            required = true
+        )
+        slug: String
+    ) {}
 
     @Command(
         name = "ide",
         aliases = ["i"],
-        description = ["opens project in configured IDE"],
+        description = ["Opens Project in configured IDE Tool"],
+        mixinStandardHelpOptions = true
     )
-    fun ide() {}
+    fun ide(
+        @Option(
+            names = ["-s", "--slug"],
+            description = [
+                "Project slug",
+            ],
+            paramLabel = "<SLUG>",
+            required = true
+        )
+        slug: String
+    ) {}
 }
 
