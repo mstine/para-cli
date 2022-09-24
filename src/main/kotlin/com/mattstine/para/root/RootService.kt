d8010f1a565ae7da85e246009d11fb212e00936b
Matt Stine <matt.stine@gmail.com>
2022-09-24 11:41:13 -0500

    implement remove and default

--- src/main/kotlin/com/mattstine/para/root/RootService.kt
+++ src/main/kotlin/com/mattstine/para/root/RootService.kt
@@ -1,6 +1,7 @@
 package com.mattstine.para.root
 
 import arrow.core.Either
+import arrow.core.None
 import java.nio.file.Path
 
 class RootService {
@@ -8,27 +9,61 @@ class RootService {
 
     fun list() = repository.toList()
 
-    fun create(name: String, path: Path): Either<RootError.Exists, Root> =
-        this.find(name).let { found ->
-            if (found != null) {
-                Either.Left(RootError.Exists)
-            } else {
-                Root(name, path).let { new ->
-                    repository += new
-                    Either.Right(new)
-                }
+    fun create(name: String, path: Path): Either<RootError, Root> =
+        if (find(name) != null) {
+            Either.Left(RootError.Exists)
+        } else if (findByPath(path) != null) {
+            Either.Left(RootError.PathExists)
+        } else {
+            Root(name, path).let { new ->
+                repository += new
+                Either.Right(new)
             }
         }
 
     fun find(name: String): Root? = repository
         .singleOrNull { it.name == name }
+
+    fun remove(name: String): Either<RootError.NotFound, None> =
+        repository.removeIf { it.name == name }
+            .let {
+                when (it) {
+                    true -> Either.Right(None)
+                    false -> Either.Left(RootError.NotFound)
+                }
+            }
+
+    fun default(name: String): Either<RootError.NotFound, None> =
+        this.find(name).let {
+            if (it != null) {
+                clearDefaultRoot()
+                it.default = true
+                Either.Right(None)
+            } else {
+                Either.Left(RootError.NotFound)
+            }
+        }
+
+    private fun clearDefaultRoot() {
+        repository.forEach { root ->
+            root.default = false
+        }
+    }
+
+    private fun findByPath(path: Path): Root? = repository
+        .singleOrNull { it.path.fileName == path.fileName }
 }
 
 data class Root(
     val name: String,
-    val path: Path
+    val path: Path,
+    var default: Boolean = false
 )
+
+
 
 sealed class RootError {
     object Exists : RootError()
+    object PathExists : RootError()
+    object NotFound : RootError()
 }
--- src/test/kotlin/com/mattstine/para/root/RootServiceTest.kt
+++ src/test/kotlin/com/mattstine/para/root/RootServiceTest.kt
@@ -34,7 +34,60 @@ class RootServiceTest {
     fun `the same root cannot be created twice`() {
         with(RootService()) {
             this.create("root1", Path.of("path1"))
-            assertThat(this.create("root1", Path.of("path1")).isLeft())
+            assertThat(this.create("root1", Path.of("path1")).isLeft()).isTrue
+        }
+    }
+
+    @Test
+    fun `the same root path cannot be referenced twice`() {
+        with(RootService()) {
+            this.create("root1", Path.of("path1"))
+            assertThat(this.create("root2", Path.of("path1")).isLeft()).isTrue
+        }
+    }
+
+    @Test
+    fun `a root can be removed`() {
+        with(RootService()) {
+            this.create("root1", Path.of("path1"))
+            assertThat(this.remove("root1").isRight()).isTrue
+        }
+    }
+
+    @Test
+    fun `a missing root cannot be removed`() {
+        with(RootService()) {
+            assertThat(this.remove("root1").isLeft()).isTrue
+        }
+    }
+
+    @Test
+    fun `a root can be made the default`() {
+        with(RootService()) {
+            this.create("root1", Path.of("path1"))
+            assertThat(this.default("root1").isRight()).isTrue
+            assertThat(this.find("root1")?.default).isTrue
+        }
+    }
+
+    @Test
+    fun `a missing root cannot be made default`() {
+        with(RootService()) {
+            assertThat(this.default("root1").isLeft()).isTrue
+        }
+    }
+
+    @Test
+    fun `only one root can be default`() {
+        with(RootService()) {
+            val root1 = this.create("root1", Path.of("path1"))
+            this.default("root1")
+
+            val root2 = this.create("root2", Path.of("path2"))
+            this.default("root2")
+
+            assertThat(root1.orNull()?.default).isFalse
+            assertThat(root2.orNull()?.default).isTrue
         }
     }
 }
